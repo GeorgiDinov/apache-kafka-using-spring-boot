127f5cce55c26be76911311d038564a5f8330e5f
Georgi Dinov <georgidinov1@abv.bg>
2022-01-19 12:30:59 +0200

    added integration test class - not working due to kafka - spring boot unresolved issues

---
+++ library-events-producer/src/test/java/com/georgidinov/libraryeventsproducer/controller/integration/LibraryEventsControllerIntegrationTest.java
@@ -0,0 +1,90 @@
+package com.georgidinov.libraryeventsproducer.controller.integration;
+
+import com.georgidinov.libraryeventsproducer.domain.Book;
+import com.georgidinov.libraryeventsproducer.domain.LibraryEvent;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.*;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
+@Profile("integration-test")
+public class LibraryEventsControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    private Consumer<Integer, String> consumer;
+
+    @BeforeEach
+    void setUp() {
+        Map<String, Object> configs = new HashMap<>(KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker));
+        consumer = new DefaultKafkaConsumerFactory<>(configs, new IntegerDeserializer(), new StringDeserializer()).createConsumer();
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown() {
+        consumer.close();
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void postLibraryEvent() throws InterruptedException {
+        //given
+        Book book = Book.builder()
+                .bookId(123)
+                .bookAuthor("Dilip")
+                .bookName("Kafka using Spring Boot")
+                .build();
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString());
+        HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent, headers);
+        //when
+        ResponseEntity<LibraryEvent> responseEntity = testRestTemplate.exchange("/v1/libraryevent", HttpMethod.POST, request, LibraryEvent.class);
+
+        //then
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        ConsumerRecord<Integer, String> consumerRecord = KafkaTestUtils.getSingleRecord(consumer, "library-events");
+        //Thread.sleep(3000);
+        String expectedRecord = "{\"libraryEventId\": null,\"book\": {\"bookId\": 456,\"bookName\": \"Kafka Using Spring Boot\",\"bookAuthor\": \"Dilip\"},\"libraryEventType\": \"NEW\"}";
+        String value = consumerRecord.value();
+
+    }
+
+}
